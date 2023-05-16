1c31ab849219c70b5ec57b2244032dbcfa11a14f
Ovadya33 <timurgts2710@gmail.com>
2023-05-16 14:48:46 +0300

    objects-conversions-operations

---
+++ app1.js
@@ -0,0 +1,57 @@
+// const x = {
+//     x: 'x',
+//     toString: function () {
+//         return 'x';
+//     },
+// };
+// const y = {
+//     y: 'y',
+//     toString: function () {
+//         return 'kuku';
+//     },
+// };
+// const d = { x: 10, y: 20 };
+// x.toString = function () {
+//     return 'xx';
+// };
+// d[x] = 100;
+// d[y] = 200;
+// const f = function () {};
+// const num = 2;
+// f.x = function (a, b) {
+//     return a + b;
+// };
+// console.log(f.x(10, 20));
+// console.log(num.x);
+// [2].x = 10;
+// console.log([2].x);
+
+// const ar = [2];
+// ar.x = 10;
+// console.log(ar.x);
+// console.log(Array.from({ length: 2 }));
+// console.log(Array.from({ length: 6 }).map((__, index) => index + 5));
+
+// console.log(
+//     Array.from({ length: 26 })
+//         .map((__, index) => String.fromCharCode(index + 'a'.charCodeAt(0)))// печататем все клавиатуру в консоле
+//         .map((s) => `<div>${s}</div>`)
+//         .join(''), // печатаем всю клавиатуру в дивах
+// );
+
+// num.x = 100
+// console.log(num.x)
+// const mm = {};
+// mm.x = 100;
+// console.log(mm.x);
+// console.log(mm);
+// // console.log(x)
+
+// let ddd;
+// // ddd.x = 200;
+
+// (1 + 2).xxxx = 1000;
+
+// let str1 = '"abcd"';
+// str1[0] = '*';
+// console.log(str1);
