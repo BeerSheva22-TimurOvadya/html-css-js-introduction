2811a0c3753a9f823aad02b64afc94d7baa9cc69
Ovadya33 <timurgts2710@gmail.com>
2023-05-17 10:27:35 +0300

    Replaced the cycle 16.05.2023

--- src/dogs.js
+++ src/dogs.js
@@ -1,25 +1,29 @@
 // var / const/ let - виды переменных
-const detailsImageElemet = document.querySelector(".details-image");
-const detailsTitleElement = document.querySelector(".details-title");
-const thumbnailsAnchors = document.querySelectorAll(".thumbnails-anchor");
-const detailsSectionElement = document.querySelector(".details-section")
-const POINT_CLASS = "is-point";
-const mainSection = document.querySelector("main");
-const HIDDEN = "hidden";
+const detailsImageElemet = document.querySelector('.details-image');
+const detailsTitleElement = document.querySelector('.details-title');
+const thumbnailsAnchors = document.querySelectorAll('.thumbnails-anchor');
+const detailsSectionElement = document.querySelector('.details-section');
+const POINT_CLASS = 'is-point';
+const mainSection = document.querySelector('main');
+const HIDDEN = 'hidden';
 
-for (let i = 0; i < thumbnailsAnchors.length; i++) {
-    thumbnailsAnchors[i].addEventListener("click", function () {
-        setDetails(thumbnailsAnchors[i]);
-    })
-}
+// for (let i = 0; i < thumbnailsAnchors.length; i++) {
+//     thumbnailsAnchors[i].addEventListener("click", function () {
+//         setDetails(thumbnailsAnchors[i]);
+//     })
+// }
+
+
+//заменили вместо цикла 16.05.2023
+thumbnailsAnchors.forEach((anchor) =>
+    anchor.addEventListener('click', setDetails.bind(undefined, anchor)),
+);
 
 function setDetails(anchor) {
     showDetails();
-    detailsImageElemet.src = anchor.getAttribute("data-details-image");
-    detailsTitleElement.innerHTML = anchor.getAttribute("data-details-text");
+    detailsImageElemet.src = anchor.getAttribute('data-details-image');
+    detailsTitleElement.innerHTML = anchor.getAttribute('data-details-text');
     detailsSectionElement.classList.add(POINT_CLASS);
-    
-    
 }
 
 function showDetails() {
@@ -27,13 +31,9 @@ function showDetails() {
 
     setTimeout(function () {
         detailsSectionElement.classList.remove(POINT_CLASS);
-    })
-    
+    });
 }
 
 function hideDetails() {
     mainSection.classList.add(HIDDEN);
-
 }
-
-
