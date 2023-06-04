f955d5ad5f34ab38b14fe3e809ca6b8c97cf1669
Ovadya33 <timurgts2710@gmail.com>
2023-06-04 20:31:02 +0300

    Employee application

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import CompanyService from './service/CompanyService.js';
-import ApplicationBar from './ui/ApplicationBar.js';
-import DataGrid from './ui/DataGrid.js';
-import EmployeeForm from './ui/EmployeeForm.js';
-import { getRandomEmployee } from './util/random.js';
-import statisticsConfig from './config/statistics-config.json' assert { type: 'json' };
-import employeesConfig from './config/employees-config.json' assert { type: 'json' };
-import { range } from './util/number-functions.js';
-import Spinner from './ui/Spinner.js';
-const N_EMPLOYEES = 3;
+import CompanyService from "./service/CompanyService.js";
+import ApplicationBar from "./ui/ApplicationBar.js";
+import DataGrid from "./ui/DataGrid.js";
+import EmployeeForm from "./ui/EmployeeForm.js";
+import { getRandomEmployee } from "./util/random.js";
+import statisticsConfig from "./config/statistics-config.json" assert{type: 'json'}
+import employeesConfig from "./config/employees-config.json" assert{type: 'json'}
+import serviceConfig from "./config/service-config.json" assert{type: 'json'}
+import { range } from "./util/number-functions.js";
+import Spinner from "./ui/Spinner.js";
+import CompanyServiceRest from "./service/CompanyServiceRest.js";
+const N_EMPLOYEES = 100;
 //employee model
 //{id: number of 9 digits, name: string, birthYear: number,
 // gender: female | male, salary: number, department: QA, Development, Audit, Accounting, Management}
 const sections = [
-    { title: 'Employees', id: 'employees-table-place' },
-    { title: 'Add Employee', id: 'employees-form-place' },
-    { title: 'Statistics', id: 'statistics-place' },
+    { title: "Employees", id: "employees-table-place" },
+    { title: "Add Employee", id: "employees-form-place" },
+    { title: "Statistics", id: "statistics-place" }
 ];
 const { minSalary, maxSalary, departments, minYear, maxYear } = employeesConfig;
 const { age, salary } = statisticsConfig;
@@ -24,49 +26,60 @@ const employeeColumns = [
     { field: 'birthYear', headerName: 'Birth Year' },
     { field: 'gender', headerName: 'Gender' },
     { field: 'salary', headerName: 'Salary (ILS)' },
-    { field: 'department', headerName: 'Department' },
+    { field: 'department', headerName: 'Department' }
+
+
 ];
 const statisticsColumns = [
-    { field: 'min', headerName: 'Min value' },
-    { field: 'max', headerName: 'Max value' },
-    { field: 'count', headerName: 'Count' },
-];
+    { field: 'min', headerName: "Min value" },
+    { field: 'max', headerName: "Max value" },
+    { field: 'count', headerName: "Count" }
+]
 //objects
-const menu = new ApplicationBar('menu-place', sections, menuHandler);
-const companyService = new CompanyService();
-const employeeForm = new EmployeeForm('employees-form-place');
-const employeeTable = new DataGrid('employees-table-place', employeeColumns, true);
-const ageStatistics = new DataGrid('age-statistics-place', statisticsColumns);
-const salaryStatistics = new DataGrid('salary-statistics-place', statisticsColumns);
-const spinner = new Spinner('spinner-place');
-
-document.getElementById('employees-table-place').insertAdjacentHTML('beforeend', '<button id="delete-button">Delete Selected</button>');
-document.getElementById('delete-button').addEventListener('click', deleteSelectedEmployees);
+const menu = new ApplicationBar("menu-place", sections, menuHandler);
+const companyService = serviceConfig.baseUrl ?new CompanyServiceRest(serviceConfig.baseUrl) : new CompanyService();
+const spinner = new Spinner("spinner-place");
+const employeeForm = new EmployeeForm("employees-form-place",employeesConfig);
+employeeForm.fillForm();
+const updateForm = new EmployeeForm("form-update-place", employeesConfig);
+updateForm.addHandler(async (empl) => {
+    const updatedEmpl = await action(companyService.updateEmployee.bind(companyService, empl));
+    employeeTable.updateRow(updatedEmpl);
+   
+    updateForm.hideForm();
+});
+const employeeTable = new DataGrid("employees-table-place", employeeColumns, {title: "List of Employees", width:"80vw"},
+ [{name: 'remove', callbackFn: removeEmployee}, {name: 'update', callbackFn: updateEmployee}]);
+const ageStatistics = new DataGrid("age-statistics-place", statisticsColumns, {title: "Age Distribution", width:"30vw"});
+const salaryStatistics = new DataGrid("salary-statistics-place", statisticsColumns, {title: "Salary Distribution", width:"30vw"});
 
 employeeForm.addHandler(async (employee) => {
+    
     await action(companyService.addEmployee.bind(companyService, employee));
-});
+})
 async function menuHandler(index) {
+    updateForm.hideForm();
     switch (index) {
         case 0: {
-            const employees = await action(companyService.getAllEmployees.bind(companyService));
+            const employees = await action(companyService.getAllEmployees
+                .bind(companyService));
             employeeTable.fillData(employees);
             break;
         }
         case 2: {
-            const ageStatisticsData = await action(
-                companyService.getStatistics.bind(companyService, age.field, age.interval),
-            );
+            const ageStatisticsData = await action(companyService.
+                getStatistics.bind(companyService, age.field, age.interval));
             ageStatistics.fillData(ageStatisticsData);
 
-            const salaryStatisticsData = await action(
-                companyService.getStatistics.bind(companyService, salary.field, salary.interval),
-            );
+            const salaryStatisticsData =
+                await action(companyService.getStatistics.bind(companyService,
+                    salary.field, salary.interval));
             salaryStatistics.fillData(salaryStatisticsData);
 
             break;
         }
     }
+
 }
 
 async function action(serviceFn) {
@@ -74,22 +87,28 @@ async function action(serviceFn) {
     const res = await serviceFn();
     spinner.stop();
     return res;
+    
 }
-function createRandomEmployees() {
-    const promises = range(0, N_EMPLOYEES).map(() =>
-        companyService.addEmployee(
-            getRandomEmployee(minSalary, maxSalary, minYear, maxYear, departments),
-        ),
-    );
-    return Promise.all(promises);
+async function updateEmployee(id) {
+    const empl = await companyService.getEmployee(id);
+    updateForm.fillForm(empl);
 }
-
-async function deleteSelectedEmployees() {
-    const selectedIds = employeeTable.getSelectedIds();
-    const promises = selectedIds.map(id => companyService.deleteEmployee(id));
-    await Promise.all(promises);
-    // Обновите таблицу работников после удаления.
-    const employees = await action(companyService.getAllEmployees.bind(companyService));
-    employeeTable.fillData(employees);
+async function removeEmployee(id) {
+    if(confirm(`Removing Employee?
+    You are going to remove employee with id=${id}`))
+    {
+        await action(companyService.removeEmployee.bind(companyService, id));
+    employeeTable.removeRow(id)
 }
+}
+async function createRandomEmployees() {
+    const employees = await companyService.getAllEmployees();
+    if (employees.length < 5) {
+        for(let i = 0; i < N_EMPLOYEES; i++) {
+            await companyService.addEmployee(getRandomEmployee(minSalary, maxSalary,
+                minYear, maxYear, departments));
+        }
+    }
+    }
+   
 action(createRandomEmployees);
--- src/ui/DataGrid.js
+++ src/ui/DataGrid.js
@@ -1,60 +1,82 @@
 export default class DataGrid {
     #tBodyElement
     #keys
-    #deletable
-    #selectAllCheckbox
-    constructor(parentId, columns, deletable = false) {
+    #actions
+    constructor(parentId, columns, caption, actions) {
         //columns - array of objects {field: <name of key>,
         // headerName: <column name>}
         this.#keys = columns.map(c => c.field);
-        this.#deletable = deletable;
-        this.#buildTableHeader(parentId, columns.map(c => c.headerName))
+        this.#buildTableHeader(parentId, caption, columns.map(c => c.headerName));
+        this.#actions = actions;
 
     }
     fillData(rowsData) {
         this.#tBodyElement.innerHTML = rowsData.map(rd => this.#getRow(rd)).join('');
+        this.setActionListeners();
+    }
+    setActionListeners() {
+        if (this.#actions) {
+            const buttons = document.querySelectorAll(`[data-button]`);
+            buttons.forEach(b => {
+                b.onclick = async () => {
+                    const idTokens = b.id.split("-", 2);
+                    const action = this.#actions.find(a => a.name == idTokens[1]);
+                    await action.callbackFn(idTokens[0]);
+                };
+            });
+        }
+    }
+
+    removeRow(id) {
+        const trElement = document.getElementById('i' + id);
+        if (trElement) {
+            trElement.remove();
+        }
+
+    }
+    updateRow(obj) {
+        const trElement = document.getElementById(`i${obj.id}`);
+        if (trElement) {
+            trElement.innerHTML = this.#getTds(obj)
+        }
+        this.setActionListeners();
+    }
+    #getTds(obj) {
+        let data = this.#keys.map(key => `<td>${obj[key]}</td>`).join('');
+        if (this.#actions) {
+            const buttons = this.#actions.map(a =>
+                `<td><button data-button id="${obj.id}-${a.name}">${a.name}</button></td>`).join('');
+                data += buttons;
+        }
+        return data;
     }
     #getRow(obj) {
-        let row = this.#keys.map(key => `<td>${obj[key]}</td>` ).join('');
-        if (this.#deletable) {
-            row = `<td><input type="checkbox" class="row-checkbox" data-id="${obj.id}" /></td>` + row;
-        }
-        return `<tr>${row}</tr>`;
+        const row = `<tr id="i${obj.id}">
+                   ${this.#getTds(obj)}
+                 </tr>  `;
+        return row;
+
+
     }
     insertRow(obj) {
-        this.#tBodyElement.innerHTML += this.#getRow(obj)
+        this.#tBodyElement.innerHTML += this.#getRow(obj);
+        this.setActionListeners();
     }
-    #buildTableHeader(parentId, columnNames) {
+    #buildTableHeader(parentId, caption, columnNames) {
         const tableSectionElement = document.getElementById(parentId);
-        let headers = columnNames.map(headerName => `<th>${headerName}</th>`).join('');
-        if (this.#deletable) {
-            headers = `<th><input type="checkbox" id="${parentId}-check-all" /></th>` + headers;
-        }
         tableSectionElement.innerHTML =
-            `<table>
-                <thead>
-                   <tr>
-                       ${headers}
-                   </tr>
-                </thead>
-                <tbody id="${parentId}-table" >
-                </tbody>
-              </table>`;
-        this.#tBodyElement = document.getElementById(parentId + "-table");
-        if (this.#deletable) {
-            this.#selectAllCheckbox = document.getElementById(parentId + "-check-all");
-            this.#selectAllCheckbox.addEventListener('change', this.#handleSelectAllChange.bind(this));
-        }
+            `<table> <caption style="width: ${caption.width}; "> ${caption.title} </caption>
+            <thead>
+               <tr>
+                   ${columnNames.map(headerName => `<th>${headerName}</th>`).join('')}
+                   ${this.#actions ?
+                Array.from({ length: this.#actions.length }.map(() => `<th></th>`)) : ''}
+               </tr>
+            </thead>
+            <tbody id="${parentId}-table" >
+            </tbody>
+          </table>`
+        this.#tBodyElement = document.getElementById(parentId + "-table")
+
     }
-    #handleSelectAllChange() {
-        const allCheckboxes = this.#tBodyElement.querySelectorAll('.row-checkbox');
-        for (const checkbox of allCheckboxes) {
-            checkbox.checked = this.#selectAllCheckbox.checked;
-        }
-    }
-    
-    getSelectedIds() {
-        const selectedCheckboxes = this.#tBodyElement.querySelectorAll('.row-checkbox:checked');
-        return Array.from(selectedCheckboxes).map(checkbox => checkbox.dataset.id);
-    }
-}    
+}
